cbd78b86566634b9591b7986ecfdd056f8761051
HectorStimer <hgoten007@gmail.com>
2026-03-24 09:55:36 -0300

    refactoring SecurityConfig, Application files and fix bugs

--- src/main/java/com/hector/encurtadorlink/config/SecurityConfig.java
+++ src/main/java/com/hector/encurtadorlink/config/SecurityConfig.java
@@ -13,15 +13,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable()) // Obrigatório para o Swagger e POSTs de teste
                 .authorizeHttpRequests(auth -> auth
+                        // libera o redirecionamento (ex: localhost:8080/abc)
                         .requestMatchers("/{shortCode}").permitAll()
+
+                        // libera TODA a infraestrutura do Swagger UI
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+
+                        // libera a rota de erro para você ver a causa real (ex: erro de banco)
+                        .requestMatchers("/error").permitAll()
+
+                        // o resto exige login
                         .anyRequest().authenticated()
-        );
+                );
         return http.build();
     }
-
 }
+
+
