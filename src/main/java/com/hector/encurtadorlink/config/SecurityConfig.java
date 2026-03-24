bd1604194f33bc21ce3f695585bcbf368b9ebbef
HectorStimer <hgoten007@gmail.com>
2026-03-24 09:14:14 -0300

    adding AsyncConfig and RedisConfig

--- src/main/java/com/hector/encurtadorlink/config/SecurityConfig.java
+++ src/main/java/com/hector/encurtadorlink/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.hector.encurtadorlink.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,13 +15,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
-                .cors(Custumizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/{shortCode}").permitAll()
                         .anyRequest().authenticated()
-                ).oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
         );
+        return http.build();
     }
 
 }
